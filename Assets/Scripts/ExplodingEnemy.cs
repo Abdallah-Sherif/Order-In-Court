a0e4a6be4dd0a6106cdb4029e77d0295726323a0
Moe <129519233+Abdallah-Moe-dev@users.noreply.github.com>
2024-02-19 13:16:04 +0200

    nav package should be working

--- Assets/Scripts/ExplodingEnemy.cs
+++ Assets/Scripts/ExplodingEnemy.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class Enemy : EnemyBehaviourScript
 {
+    [SerializeField] GameObject fire_VFX;
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.CompareTag("Player"))
         {
             Destroy(gameObject);
+            GameObject explode = Instantiate(fire_VFX, transform.position, transform.rotation);
         }
     }
 }
